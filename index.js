9deda81dcfd2123210e7545b4e20c87f48e6e7eb
Vanessa Wagner <vmwagner@gmail.com>
2022-05-18 18:02:23 -0500

    commit tree 30cb4f4445607a9c0815a8bfeed50ea566e59189

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+
+    //console.log(customerName)
+    customerName = customerName.toUpperCase()
+    //console.log(customerName)
+    return customerName
+}
+//upperCaseCustomerName()
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'some value'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'something else'
+    return leastFavoriteCustomer
+}
